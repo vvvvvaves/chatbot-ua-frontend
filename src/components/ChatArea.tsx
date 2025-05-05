41282fe4206625f7924731b98ca2f4972f113aff
Vik <shkurenkov18@gmail.com>
2025-05-05 18:57:29 +0200

    Format input field and the greetings on the new chat

--- src/components/ChatArea.tsx
+++ src/components/ChatArea.tsx
@@ -3,7 +3,7 @@ import "./ChatArea.css";
 function ChatArea() {
   return <div className="chat-area">
     <h1 className="greeting-h1">Вітаємо!</h1>
-    <h2 className="greeting-h2">Цей чат відповідає на питання стосовно життя та легалізації українців у Польщі</h2>
+    <h2 className="greeting-h2">Цей чат відповідає на питання щодо життя та легалізації українців у Польщі</h2>
     <input className="chat-input"/>
   </div>;
 }
