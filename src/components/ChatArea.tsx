6202926e5b7d104a54b60aba22d9733261f80731
Vik <shkurenkov18@gmail.com>
2025-05-05 19:13:06 +0200

    Karina suggested one-line h2

--- src/components/ChatArea.tsx
+++ src/components/ChatArea.tsx
@@ -3,7 +3,9 @@ import "./ChatArea.css";
 function ChatArea() {
   return <div className="chat-area">
     <h1 className="greeting-h1">Вітаємо!</h1>
-    <h2 className="greeting-h2">Цей чат відповідає на питання щодо життя та легалізації українців у Польщі</h2>
+    {/* <h2 className="greeting-h2">Цей чат відповідає на питання щодо життя та легалізації українців у Польщі</h2> */}
+    <h2 className="greeting-h2">Маєте питання стосовно життя у Польщі?</h2>
+
     <input className="chat-input"/>
   </div>;
 }
