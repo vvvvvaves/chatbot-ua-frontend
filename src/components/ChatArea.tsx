206f4dd3ceade959f6985a6c825b00d9e3375708
Vik <shkurenkov18@gmail.com>
2025-05-06 21:07:00 +0200

    Chat input text update

--- src/components/ChatArea.tsx
+++ src/components/ChatArea.tsx
@@ -5,8 +5,7 @@ function ChatArea() {
     <h1 className="greeting-h1">Вітаємо!</h1>
     {/* <h2 className="greeting-h2">Цей чат відповідає на питання щодо життя та легалізації українців у Польщі</h2> */}
     <h2 className="greeting-h2">Маєте питання стосовно життя у Польщі?</h2>
-
-    <input className="chat-input"/>
+    <textarea placeholder={"Чи можна виїжджати з Польщі в процесі отримання карти побиту?.."} className="chat-input" />
   </div>;
 }
 
