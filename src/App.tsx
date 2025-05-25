aadc873fb2edd28e26cbbaf4d0f09adfe07f440b
Vik <shkurenkov18@gmail.com>
2025-05-25 22:53:12 +0200

    Changed root to new-chat

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import "./App.css";
 import Sidebar from "./components/Sidebar";
 import ChatPage from "./pages/ChatPage";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
 import Donate from "./pages/Donate";
 import About from "./pages/About";
 import Settings from "./pages/Settings";
@@ -13,7 +13,8 @@ function App() {
         <Sidebar />
         <div className="content">
           <Routes>
-            <Route path="/" element={<ChatPage />} />
+            <Route path="/" element={<Navigate to="/new-chat" replace={true} />} />
+            <Route path="/new-chat" element={<ChatPage />} />
             <Route path="/donate" element={<Donate />} />
             <Route path="/about" element={<About />} />
             <Route path="/settings" element={<Settings/>} />
